3c28712287a39d27f92eea220bf43ef864b7d689
jmooney <john_mooney@optum.com>
2021-10-09 13:35:21 -0400

    Initial project add.  Closes #1.

--- src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
+++ src/test/java/guru/springframework/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -1,14 +1,13 @@
 package guru.springframework.msscbeerservice;
 
-
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-public class MsscBeerServiceApplicationTests {
+class MsscBeerServiceApplicationTests {
 
-    @Test
-    public void contextLoads() {
-    }
+	@Test
+	void contextLoads() {
+	}
 
 }
