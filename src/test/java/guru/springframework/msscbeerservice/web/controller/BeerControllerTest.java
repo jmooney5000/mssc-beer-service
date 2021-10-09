66e5ade8999ccdf9d04786f590bf0e4bd698e6f4
jmooney <john_mooney@optum.com>
2021-10-09 18:55:45 -0400

    Create JUnit 5 Tests for Beer Service Controller...  Closes #5.

--- src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
+++ src/test/java/guru/springframework/msscbeerservice/web/controller/BeerControllerTest.java
@@ -8,7 +8,9 @@ import org.springframework.test.web.servlet.MockMvc;
 
 import java.util.UUID;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 
 @WebMvcTest(BeerController.class)
 class BeerControllerTest {
